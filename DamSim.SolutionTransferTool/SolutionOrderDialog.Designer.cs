a22eb7c1dfe035a0fc3c16f8664da316db2e870a
Tanguy TOUZARD <tanguy92@hotmail.com>
2016-05-16 14:58:01 +0200

    Initial commit Fix #1 Added multi solutions import capability

---
+++ DamSim.SolutionTransferTool/SolutionOrderDialog.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace DamSim.SolutionTransferTool
+{
+    partial class SolutionOrderDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SolutionOrderDialog));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.lvSolutions = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tsbDown = new System.Windows.Forms.ToolStripButton();
+            this.tsbUp = new System.Windows.Forms.ToolStripButton();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1057, 60);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(568, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Organize solutions in the order they need to be imported";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnOk);
+            this.panel2.Controls.Add(this.btnCancel);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 528);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1057, 60);
+            this.panel2.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbDown,
+            this.tsbUp});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 60);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1057, 32);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // lvSolutions
+            // 
+            this.lvSolutions.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lvSolutions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvSolutions.FullRowSelect = true;
+            this.lvSolutions.Location = new System.Drawing.Point(0, 92);
+            this.lvSolutions.Name = "lvSolutions";
+            this.lvSolutions.Size = new System.Drawing.Size(1057, 436);
+            this.lvSolutions.TabIndex = 4;
+            this.lvSolutions.UseCompatibleStateImageBehavior = false;
+            this.lvSolutions.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Solution";
+            this.columnHeader1.Width = 200;
+            // 
+            // tsbDown
+            // 
+            this.tsbDown.Image = ((System.Drawing.Image)(resources.GetObject("tsbDown.Image")));
+            this.tsbDown.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDown.Name = "tsbDown";
+            this.tsbDown.Size = new System.Drawing.Size(79, 29);
+            this.tsbDown.Text = "Down";
+            this.tsbDown.Click += new System.EventHandler(this.tsbDown_Click);
+            // 
+            // tsbUp
+            // 
+            this.tsbUp.Image = ((System.Drawing.Image)(resources.GetObject("tsbUp.Image")));
+            this.tsbUp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbUp.Name = "tsbUp";
+            this.tsbUp.Size = new System.Drawing.Size(55, 29);
+            this.tsbUp.Text = "Up";
+            this.tsbUp.Click += new System.EventHandler(this.tsbUp_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.Location = new System.Drawing.Point(810, 11);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(112, 35);
+            this.btnOk.TabIndex = 7;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(932, 11);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(112, 35);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // SolutionOrderDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 588);
+            this.Controls.Add(this.lvSolutions);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SolutionOrderDialog";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Load += new System.EventHandler(this.SolutionOrderDialog_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbDown;
+        private System.Windows.Forms.ListView lvSolutions;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ToolStripButton tsbUp;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ DamSim.SolutionTransferTool/SolutionOrderDialog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DamSim.SolutionTransferTool
+{
+    public partial class SolutionOrderDialog : Form
+    {
+        public SolutionOrderDialog(List<string> solutions)
+        {
+            InitializeComponent();
+
+            Solutions = solutions;
+        }
+
+        public List<string> Solutions { get; private set; }
+
+        private void tsbDown_Click(object sender, EventArgs e)
+        {
+            if (lvSolutions.SelectedItems[0].Index == lvSolutions.Items.Count - 1)
+                return;
+
+            var currentIndex = lvSolutions.SelectedItems[0].Index;
+            var item = lvSolutions.Items[currentIndex];
+            lvSolutions.Items.RemoveAt(currentIndex);
+            lvSolutions.Items.Insert(currentIndex + 1, item);
+        }
+
+        private void tsbUp_Click(object sender, EventArgs e)
+        {
+            if (lvSolutions.SelectedItems[0].Index == 0)
+                return;
+
+            var currentIndex = lvSolutions.SelectedItems[0].Index;
+            var item = lvSolutions.Items[currentIndex];
+            lvSolutions.Items.RemoveAt(currentIndex);
+            lvSolutions.Items.Insert(currentIndex - 1, item);
+        }
+
+        private void SolutionOrderDialog_Load(object sender, EventArgs e)
+        {
+            foreach(var solution in Solutions)
+            {
+                var item = new ListViewItem(solution);
+
+                lvSolutions.Items.Add(item);
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Solutions.Clear();
+            for(var i=0; i< lvSolutions.Items.Count; i++)
+            {
+                Solutions.Add(lvSolutions.Items[i].Text);
+            }
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
